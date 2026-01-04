83a66bfb57c5ce40114c67007a118d1be21b8931
satvik <satvikrajput32@gmail.com>
2026-01-04 15:59:25 +0530

    added streaming to the chatbot

--- backend.py
+++ backend.py
@@ -1,6 +1,6 @@
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, List, Annotated
-from langchain_core.messages import BaseMessage 
+from langchain_core.messages import BaseMessage, HumanMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph.message import add_messages
@@ -26,4 +26,20 @@ graph = StateGraph(chatState)
 graph.add_node("chat_node", chat_node)
 graph.add_edge(START, "chat_node")
 graph.add_edge("chat_node", END)
+
 chatbot = graph.compile(checkpointer=checkpointer)
+
+"""
+------------------------
+Streaming Implementation
+------------------------
+
+for message_chunk, metadata in chatbot.stream(
+    {'messages': [HumanMessage(content="write a 50 words notice informing the students of school that tomorrow is a holdiday")]}, 
+    config= {"configurable" : {"thread_id" : "thread_1"}},
+    stream_mode="messages"               
+):
+    if message_chunk.content:
+        print(message_chunk.content, end = " ", flush=True)
+
+"""
--- frontend.py
+++ frontend.py
@@ -20,11 +20,12 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = chatbot.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
-    
-    ai_message = response['messages'][-1].content
-
-    # first add the message to message_history
-    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    stream = chatbot.stream(
+        {'messages': [HumanMessage(content=user_input)]}, 
+        config= CONFIG,
+        stream_mode="messages"               
+    )
     with st.chat_message('assistant'):
-        st.text(ai_message)
+        ai_message = st.write_stream(message_chunk for message_chunk, metadata in stream)
+    
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
